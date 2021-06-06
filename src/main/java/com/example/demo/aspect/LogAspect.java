09cca0b76ab38a17c90b22e8271604773008abe8
wuyukang <wuyukangkang@outlook.com>
2021-06-07 07:59:03 +0800

    使用注解实现 AOP

--- src/main/java/com/example/demo/aspect/LogAspect.java
+++ src/main/java/com/example/demo/aspect/LogAspect.java
@@ -1,27 +1,22 @@
-package com.example.demo;
+package com.example.demo.aspect;
 
+import com.example.demo.annotation.Log;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LogAspect {
-    @Pointcut("execution(* com.example.demo.HelloController.*(..))")
-    public void printLog() {
-
-    }
-
-    @Before("printLog()")
-    public void before(JoinPoint joinPoint) {
+    @Before("@annotation(log)")
+    public void before(JoinPoint joinPoint, Log log) {
         System.out.println("Before ... " + joinPoint.getKind() + "," + joinPoint.toLongString());
     }
 
-    @After("printLog()")
-    public void after(JoinPoint joinPoint) {
+    @After("@annotation(log)")
+    public void after(JoinPoint joinPoint, Log log) {
         System.out.println("After ... " + joinPoint.getKind() + "," + joinPoint.toLongString());
     }
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.annotation.Log;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    @Log("hello")
     @GetMapping("/hello")
     public void hello() {
         System.out.println("hello");
